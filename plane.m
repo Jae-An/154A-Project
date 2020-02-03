d87d2b33006b1396a4bcd802127195c3723ae426
Jae-An <jjwang319@gmail.com>
2020-02-02 22:40:13 -0800

    Create plane.m

---
+++ plane.m
@@ -0,0 +1,19 @@
+function plane = plane()
+%Creates an instance of the rocket class
+
+plane = struct('geo',[],'prop',[],'data',[]);
+
+%% Aircraft Geometry
+plane.geo = struct('body',[],'wing',[],'h_tail',[],'v_tail',[],'nacelle',[]);
+    plane.geo.body = struct('L',[],'W',[],'D',[]);
+    plane.geo.wing = struct('S',[],'AR',[],'c',[],'b',[],'thickness',[],'taper',[],'sweep',[],'cg',[],'ac',[],'h_cg',[],'h_ac',[]);
+    plane.geo.h_tail = struct('S',[],'AR',[],'c',[],'b',[],'thickness',[],'taper',[],'sweep',[],'cg',[],'ac',[],'h_cg',[],'h_ac',[]);
+    plane.geo.v_tail = struct('S',[],'AR',[],'c',[],'b',[],'thickness',[],'taper',[],'sweep',[],'cg',[],'ac',[],'h_cg',[],'h_ac',[]);
+    plane.geo.nacelle = struct(); % *fill out later*
+
+%% Propulsion
+plane.prop = struct('W',[],'hp',[],'eta_p',[],'c_p',[]);
+
+
+%% Performance, Aerodynamics, Stability
+plane.data = struct('',[],'',[],'',[]); % *fill out later*
