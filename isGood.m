0469c068ee0be56bf40045479d195f1bb098bd16
Alexander Cusianovic <acusianovic@gmail.com>
2020-03-07 18:13:07 -0800

    Cleaned up main and isGood functions

--- isGood.m
+++ isGood.m
@@ -1,4 +1,4 @@
-function [rocGood, VcGood, GeoIsGood] = isGood(plane)
+function [planeGood, rocGood, VcGood, GeoIsGood] = isGood(plane)
 %% identifying good RoC
 if plane.data.performance.ROC >= 33
     rocGood = true;
@@ -8,13 +8,15 @@ end
 
 
 %% V_cruise good
-if plane.data.aero.v_cruise >= 220
+if plane.data.aero.v_cruise(1) >= 220 && plane.data.aero.v_cruise(2) >= 220
     VcGood = true;
 else
     VcGood = false;
 end
 
 %% isStable
+
+
 
 
 
@@ -28,6 +30,13 @@ GeoIsGood = false;
 if wing.S > h_tail.S %checking if wing area is greater than tail area
     GeoIsGood = true;
 end
+
+%% Overall Good
+planeGood = false;
+if GeoIsGood && rocGood && VcGood && plane.data.stability.is_stable && plane.data.aero.isreal
+    planeGood = true;
+end
+
 
 end
 
